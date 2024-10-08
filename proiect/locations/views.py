dc8821601a9d5c4f88dd050d4cb423fe8a99aaf2
Catalin Bulgar <catalin.bulgar@gmail.com>
2024-10-08 21:31:32 +0300

    part 2 django

--- proiect/locations/views.py
+++ proiect/locations/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 from locations.models import Location
 
@@ -11,4 +11,28 @@ class CreateLocationView(CreateView):
     fields = ['city', 'country']
 
     def get_success_url(self):
-        return reverse('locations:adaugare')
+        return reverse('locations:lista_locatii')
+
+
+class LocationView(ListView):
+    model = Location
+    template_name = 'locations/location_index.html'
+
+
+class UpdateLocationView(UpdateView):
+    model = Location
+    fields = ['city', 'country']
+    template_name = 'forms.html'
+
+    def get_success_url(self):
+        return reverse('locations:lista_locatii')
+
+
+def deactivate_location(request, pk):
+    Location.objects.filter(id=pk).update(active=False)
+    return redirect('locations:lista_locatii')
+
+
+def activate_location(request, pk):
+    Location.objects.filter(id=pk).update(active=True)
+    return redirect('locations:lista_locatii')
--- proiect/proiect/settings.py
+++ proiect/proiect/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EUROPE/Bucharest'
 
 USE_I18N = True
 
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/locations/'
+LOGOUT_REDIRECT_URL = '/'
